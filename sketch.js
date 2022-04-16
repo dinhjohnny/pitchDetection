bb0f9656a4482bce9ddf9a5fa1bc9aebcd68d020
jdinhhh <jayteed01@gmail.com>
2022-04-15 20:45:30 -0700

    Implemented note recognition
    - Of the 6 notes of standard guitar tuning (EADGBE), the note that is closest to the frequency being received is displayed on the canvas

--- sketch.js
+++ sketch.js
@@ -7,6 +7,14 @@ let mic;
 let frequency = 0;
 let threshold = 1;
 
+let notes = 
+  {'low_e' : 82.41,
+   'a' : 110.00,
+   'd' : 146.83,
+   'g' : 196.00,
+   'b' : 246.94,
+   'high_e' : 329.63
+  }
 
 function setup() {
   createCanvas(400, 400);
@@ -41,4 +49,21 @@ function modelLoaded() {
 
 function draw() {
   background(220);
+  
+  let note = 1;
+  let smallest_diff = Infinity;
+  for (var n in notes) {
+    if (Math.abs(frequency - notes[n]) < smallest_diff)
+    {
+      note = n;
+      smallest_diff = Math.abs(frequency - notes[n])
+    }
+  }
+  
+  if (smallest_diff < 30) {
+    textAlign(CENTER, CENTER);
+    fill(255);
+    textSize(64);
+    text(note, width / 2, height - 50);
+  }
 }
